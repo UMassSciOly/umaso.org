922e5147bf46fda630e4f9e34552aa1c42ff4a44
vc64 <vichen2004@gmail.com>
2024-09-09 15:08:39 -0400

    add about page with mission statement that collapses on mobile, add styled buttons for profile section

---
+++ js/about.js
@@ -0,0 +1,6 @@
+function toggle_mission() {
+    mission_details = document.getElementById("mission-details");
+    mission_toggle = document.getElementById("mission-toggle");
+    mission_details.classList.toggle("active");
+    mission_toggle.classList.toggle("active");
+}
