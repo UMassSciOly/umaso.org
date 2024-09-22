4324ab8c7af135a5f5643350c6f270ae0aa63d8e
vc64 <vichen2004@gmail.com>
2024-09-22 15:55:22 -0400

    add dynamic loading of profiles for organizers and event members based on provided CSVs, create python script for compressing and looking up pfp images present

--- make_pfp_list.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-directory = './img/pfp'
-files = os.listdir(directory)
-
-with open('pfp_img_list.json', 'w') as f:
-    json.dump({file.split(".")[0]:file.split(".")[1] for file in files if "." in file}, f)
