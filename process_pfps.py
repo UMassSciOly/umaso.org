82b58d9019c5f3ff7ccf88eca31f4abda92b7912
vc64 <vichen2004@gmail.com>
2024-11-29 23:50:17 -0500

    add new COs to about page

--- process_pfps.py
+++ process_pfps.py
@@ -7,7 +7,7 @@ directory = './img/pfp'
 files = os.listdir(directory)
 
 with open('pfp_img_list.json', 'w') as f:
-    json.dump({file.split(".")[0]:file.split(".")[1] for file in files if "." in file}, f)
+    json.dump({file.split(".")[0]:True for file in files if "." in file}, f)
 
 # compress photos
 
